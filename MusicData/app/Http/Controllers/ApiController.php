39beca1c2790191242b9cd6c95876d23a0b70308
Adam Ankeli <ankeliadam@gmail.com>
2020-12-11 15:52:19 +0200

    Create music record via api

---
+++ MusicData/app/Http/Controllers/ApiController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Music;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function getAllMusic()
+    {
+        // use to get all Music records
+        
+    }
+
+    public function createMusic(Request $request)
+    {
+        // use to create a music record 
+        $music = new Music;
+        $music->album = $request->album;
+        $music->artist = $request->artist;
+        $music->genre = $request->genre;
+        $music->tags = $request->tags;
+        $music->price = $request->price;
+        $music->save();
+
+        return response()->json([
+            "message" => "Music record created Successfully"
+        ], 201);
+    }
+
+    public function getMusic($id)
+    {
+        // use to retrieve music record
+    }
+
+    public function updateMusic(Request $request, $id)
+    {
+        // use to update music record
+    }
+
+    public function deleteMusic($id)
+    {
+        //use to delete music record
+    }
+}
