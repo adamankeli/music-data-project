db05eeab4782a87da4870e6febcd964f4eac3b9a
Adam Ankeli <ankeliadam@gmail.com>
2020-12-12 10:57:59 +0200

    Excel file import controller

--- MusicData/app/Http/Controllers/ImportExcel/ImportExcelController.php
+++ MusicData/app/Http/Controllers/ImportExcel/ImportExcelController.php
@@ -4,8 +4,26 @@ namespace App\Http\Controllers\ImportExcel;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Imports\ImportMusic;
+use Maatwebsite\Excel\Facades\Excel;
+use DB;
+use App\Music;
 
 class ImportExcelController extends Controller
 {
     //
+    public function index()
+    {
+        $contacts = Music::orderBy('created_at', 'DESC')->get();
+        return view('import_excel.index', compact('music'));
+    }
+
+    public function import(Request $request)
+    {
+        $request->validate([
+            'import_file' => 'required'
+        ]);
+        Excel::import(new ImportMusic, request()->file('import_file'));
+        return back()->with('success', 'Contacts imported successfully.');
+    }
 }
